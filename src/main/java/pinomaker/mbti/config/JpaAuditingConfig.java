9482ea87854c532a2a9012072fead4e4bcf13dde
pinomaker-hoo <inhoo23@naver.com>
2023-04-03 22:43:19 +0900

    feat : Common Dto, Entity

---
+++ src/main/java/pinomaker/mbti/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package pinomaker.mbti.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
