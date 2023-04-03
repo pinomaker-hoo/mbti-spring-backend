514f90b94a9b46f7d753c5d84507f0298b764bc3
pinomaker-hoo <inhoo23@naver.com>
2023-04-03 22:47:00 +0900

    feat : Swagger Set

---
+++ src/main/java/pinomaker/mbti/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package pinomaker.mbti.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("MBTI")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("MBTI API")
+                        .description("Do u Know My MBTI??")
+                        .version("v0.0.1"));
+    }
+}
