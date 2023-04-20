2367d5f258db11842a0b58937f0c7445dfdeced7
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-04-21 00:52:33 +0900

    fix : Web Config Cors

--- src/main/java/pinomaker/mbti/config/WebConfig.java
+++ src/main/java/pinomaker/mbti/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("https://mbti.swygbro.com")
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
